1282f11d960bb533ec7794022a6b58a439f39ee8
fayyez <fayyez2056@gmail.com>
2026-03-18 01:05:44 +0500

    outline diagnosis endpoint

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -1,9 +1,14 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import PatientViewSet, VitalsViewSet
+from .views import PatientViewSet, VitalsViewSet, diagnose_with_medgemma
 
 router = DefaultRouter()
 router.register(r"patients", PatientViewSet, basename="patient")
 router.register(r"vitals", VitalsViewSet, basename="vitals")
+
+urlpatterns = [
+    # diagnois endpoints
+    path("diagnose/", views.diagnose_with_medgemma, name="diagnose"),
+]
 
 urlpatterns = router.urls
